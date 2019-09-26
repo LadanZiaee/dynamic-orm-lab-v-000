e96018672c6f63a36460fc03244913138bd430c1
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-26 16:16:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,5 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-
+  def self.find_by(key)
+    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    DB[:conn].execute(sql, key)
+  end
+  
 end
